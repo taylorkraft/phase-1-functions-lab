554cfda2ce647ef77f8f07a52589d9c1a679cce6
taylor kraft <taylorkraft6@gmail.com>
2021-11-11 22:07:54 -0700

    declare functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(whateverValue) {
+  //returns the number of blocks given a value
+}
+
+function distanceFromHqInFeet(whateverValue) {
+  distanceFromHqInBlocks(whateverValue)
+  // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+  // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+
+  // the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+
+function distanceTraveledInFeet(startPoint, endPoint) {
+  //returns the value of feet traveled
+}
+
+function calculatesFarePrice(startPoint, endPoint) {
+  //returns the fare for the customer
+}
